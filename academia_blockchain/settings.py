7b429a5fc0a0df9b843b7a6c02b9f7de1562c5d3
Alejandro <alejoveintimilla@gmail.com>
2020-06-25 15:10:54 -0500

    requirements

--- academia_blockchain/settings.py
+++ academia_blockchain/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
