fee30cd1d7c8450ae24ca59a69f896c30aa17761
Alejandro Veintimilla <alejoveintimilla@gmail.com>
2020-12-02 13:17:08 -0500

    remover app exams

--- academia_blockchain/settings.py
+++ academia_blockchain/settings.py
@@ -40,8 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'profiles',
-    'courses',
-    'exams'
+    'courses'
 ]
 
 MIDDLEWARE = [
--- exams/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-
-from exams.models import Exam, ExamTaken
-
-admin.site.register(Exam)
-admin.site.register(ExamTaken)
