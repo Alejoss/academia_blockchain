b6caf9cce2fe5a3c66b1e844e2a5efae8b40a9ad
Nicolas Diz <diz.nicolasamor@gmail.com>
2021-01-07 00:41:50 -0300

    FRONT-0001: finish ertificate_preview proposal

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     },
     mode: 'development',
     output: {
-        path: path.resolve(__dirname, '../static/assets//bundles'),
+        path: path.resolve(__dirname, '../static/assets/build'),
         filename: '[name].bundle.js',
     },
     module: {
