f4c3b1446cceaec5ccfffe123b0164520fe731ca
Alejandro Veintimilla <alejoveintimilla@gmail.com>
2021-01-27 13:26:46 -0500

    rename static files folder

--- academia_blockchain/settings.py
+++ academia_blockchain/settings.py
@@ -116,7 +116,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR + "/media/"
 STATIC_ROOT = 'staticfiles/'
